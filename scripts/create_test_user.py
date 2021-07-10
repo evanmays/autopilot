af894b47427f5a23b26c8d78226f4f156b5a8eda
Evan Mays <me@evanmays.com>
2021-07-10 10:43:36 -0700

    Added script for screating test user

---
+++ scripts/create_test_user.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from web3 import Web3
+import random
+import string
+
+def createNewUser():
+    # connect to hardhat
+    w3 = Web3()
+
+    # create account
+    entropy = ''.join(random.choice(string.ascii_lowercase) for i in range(32))
+    myAccount = w3.eth.account.create(entropy)
+    newAddress = myAccount.address
+    newPrivateKey = myAccount.privateKey.hex()
+
+    # transfer from root account to new account
+    w3.eth.send_transaction({'to': newAddress, 'value': int(1e18)}) # Idk where the money's coming from but I think this function breaks after about 1000 calls
+    assert w3.eth.get_balance(newAddress) > 0
+
+    return newAddress, newPrivateKey
+
+if __name__ == "__main__":
+    public, private = createNewUser()
+    print(f"       ========= Generating New User =========")
+    print(f"Public Address:   {public}")
+    print(f"Private Key:      {private}")
